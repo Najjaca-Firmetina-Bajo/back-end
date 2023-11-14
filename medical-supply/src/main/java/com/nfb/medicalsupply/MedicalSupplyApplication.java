d610a20c10a3092ecb57fc7c0d4ab79d877ebb91
Uslje <147374020+uslje26@users.noreply.github.com>
2023-11-14 23:26:02 +0100

    feat: initial-commit

---
+++ medical-supply/src/main/java/com/nfb/medicalsupply/MedicalSupplyApplication.java
@@ -0,0 +1,13 @@
+package com.nfb.medicalsupply;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalSupplyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalSupplyApplication.class, args);
+	}
+
+}
---
+++ medical-supply/src/test/java/com/nfb/medicalsupply/MedicalSupplyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nfb.medicalsupply;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalSupplyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
