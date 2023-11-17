90ee49ae14dcdb088185e6928b9ae32fc99452bf
somelijer <116906162+somelijer@users.noreply.github.com>
2023-11-17 19:22:59 +0100

    fix: CORS

---
+++ medical-supply/src/main/java/com/nfb/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.nfb.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("http://localhost:4200") // Replace with your actual client origin
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("Content-Type", "Authorization")
+                .allowCredentials(true);
+    }
+}
+
