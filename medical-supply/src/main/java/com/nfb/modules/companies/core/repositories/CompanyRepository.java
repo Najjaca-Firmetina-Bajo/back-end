6a30fbd32f062d330d7e3d5381ec8d26e808d80c
Stefaan01 <116906194+Stefaan01@users.noreply.github.com>
2023-11-18 15:58:47 +0100

    fix: repository annotation

--- medical-supply/src/main/java/com/nfb/modules/companies/core/repositories/CompanyRepository.java
+++ medical-supply/src/main/java/com/nfb/modules/companies/core/repositories/CompanyRepository.java
@@ -2,9 +2,11 @@ package com.nfb.modules.companies.core.repositories;
 
 import com.nfb.modules.companies.core.domain.company.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface CompanyRepository extends JpaRepository<Company, Long> {
     List<Company> findByIdIn(List<Long> ids);
 }
--- medical-supply/src/main/java/com/nfb/modules/companies/core/repositories/EquipmentRepository.java
+++ medical-supply/src/main/java/com/nfb/modules/companies/core/repositories/EquipmentRepository.java
@@ -2,9 +2,11 @@ package com.nfb.modules.companies.core.repositories;
 
 import com.nfb.modules.companies.core.domain.equipment.Equipment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface EquipmentRepository extends JpaRepository<Equipment, Long> {
     List<Equipment> findByTypeContainingIgnoreCase(String type);
     List<Equipment> findByNameContainingIgnoreCase(String name);
