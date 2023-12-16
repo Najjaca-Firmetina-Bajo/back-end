a59722ea052535a142bae3c1687fa21faa1db928
Stefaan01 <116906194+Stefaan01@users.noreply.github.com>
2023-12-16 21:59:47 +0100

    feat: appointment and registered user relationship

--- medical-supply/src/main/java/com/nfb/modules/stakeholders/core/domain/user/RegisteredUser.java
+++ medical-supply/src/main/java/com/nfb/modules/stakeholders/core/domain/user/RegisteredUser.java
@@ -1,9 +1,10 @@
 package com.nfb.modules.stakeholders.core.domain.user;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.DiscriminatorValue;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import com.nfb.modules.companies.core.domain.appointment.Appointment;
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "RegisteredUser")
@@ -11,15 +12,26 @@ public class RegisteredUser extends User {
 
     @Column(name = "penal_points")
     private int penalPoints;
+    @OneToMany(mappedBy = "registeredUser", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    private List<Appointment> appointments;
 
     public RegisteredUser(String email, String password, UserRole role, String name, String surname, String city,
                           String country, String phoneNumber, String occupation, String companyInfo, int penalPoints) {
         super(email, password, UserRole.RegisteredUser, name, surname, city, country, phoneNumber, occupation, companyInfo);
         this.penalPoints = penalPoints;
+        this.appointments = new ArrayList<>();
     }
 
     public RegisteredUser() {
         // Default constructor required by JPA
+    }
+
+    public List<Appointment> getAppointments() {
+        return appointments;
+    }
+
+    private void setAppointments(List<Appointment> appointments) {
+        this.appointments = appointments;
     }
 
     public int getPenalPoints() {
