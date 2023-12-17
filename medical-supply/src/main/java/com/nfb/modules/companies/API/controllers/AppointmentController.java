f97a31ff33858f307b679d2a61b78de48710dcfa
somelijer <116906162+somelijer@users.noreply.github.com>
2023-12-17 20:48:56 +0100

    feat: added appointment filtering by company

--- medical-supply/src/main/java/com/nfb/modules/companies/API/controllers/AppointmentController.java
+++ medical-supply/src/main/java/com/nfb/modules/companies/API/controllers/AppointmentController.java
@@ -3,9 +3,11 @@ package com.nfb.modules.companies.API.controllers;
 import com.nfb.modules.companies.API.dtos.AppointmentDto;
 import com.nfb.modules.companies.API.dtos.CompanyDto;
 import com.nfb.modules.companies.core.domain.appointment.Appointment;
+import com.nfb.modules.companies.core.domain.calendar.WorkingDay;
 import com.nfb.modules.companies.core.domain.company.Company;
 import com.nfb.modules.companies.core.usecases.AppointmentService;
 import com.nfb.modules.companies.core.usecases.CompanyService;
+import com.nfb.modules.companies.core.usecases.WorkingDayService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,9 +23,12 @@ public class AppointmentController {
 
     @Autowired
     private AppointmentService appointmentService;
+    @Autowired
+    private WorkingDayService workingDayService;
 
-    public AppointmentController(AppointmentService appointmentService) {
+    public AppointmentController(AppointmentService appointmentService,WorkingDayService workingDayService) {
         this.appointmentService = appointmentService;
+        this.workingDayService = workingDayService;
     }
 
     @GetMapping("/findBy/{workingDayId}")
@@ -47,6 +52,23 @@ public class AppointmentController {
         List<AppointmentDto> dtos = new ArrayList<>();
         for (Appointment a : appointments) {
             dtos.add(new AppointmentDto(a));
+        }
+
+        return new ResponseEntity<>(dtos, HttpStatus.OK);
+    }
+
+    @GetMapping("/get-all-calendar/{id}")
+    public ResponseEntity<List<AppointmentDto>> findByCompany(@PathVariable long id) {
+
+        List<Appointment> appointments = appointmentService.getAll();
+        List<WorkingDay> days = workingDayService.getBy(id);
+
+        List<AppointmentDto> dtos = new ArrayList<>();
+        for (Appointment a : appointments) {
+            if(days.stream().anyMatch(item -> a.getWorkingDay().getId() ==item.getId() ) ){
+                dtos.add(new AppointmentDto(a));
+            }
+
         }
 
         return new ResponseEntity<>(dtos, HttpStatus.OK);
