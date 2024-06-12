27c98012b0d4c3c4c8f7d811d51a9525a3b7dc71
Uslje <147374020+uslje13@users.noreply.github.com>
2024-06-12 13:44:04 +0200

    feat: get all reservation by admin id

--- medical-supply/src/main/java/com/nfb/modules/companies/core/repositories/QREqipmentRepository.java
+++ medical-supply/src/main/java/com/nfb/modules/companies/core/repositories/QREqipmentRepository.java
@@ -2,8 +2,14 @@ package com.nfb.modules.companies.core.repositories;
 
 import com.nfb.modules.companies.core.domain.appointment.QREquipment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface QREqipmentRepository extends JpaRepository<QREquipment, Long> {
     boolean existsByEquipment_Id(Long equipmentId);
+    @Query("SELECT qe FROM QREquipment qe JOIN qe.qrCode qc JOIN qc.appointment a WHERE a.companyAdministrator.id = :companyAdministratorId")
+    List<QREquipment> findAllByCompanyAdministratorId(@Param("companyAdministratorId") Long companyAdministratorId);
 
 }
